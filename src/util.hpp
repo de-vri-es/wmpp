008ae769f79dfd9cc09f108a6e1f7f6146843f84
Maarten de Vries <maarten@de-vri.es>
2016-11-24 18:57:48 +0100

    Comment util.hpp.

--- src/util.hpp
+++ src/util.hpp
@@ -4,6 +4,7 @@
 
 namespace wmpp {
 
+/// Deleter that calls std::free.
 struct free_deleter {
 	template<typename T>
 	void operator() (T * pointer) {
@@ -11,9 +12,11 @@ struct free_deleter {
 	}
 };
 
+/// A unique_ptr using free_deleter.
 template<typename T>
 using free_pointer = std::unique_ptr<T, free_deleter>;
 
+/// Wrap a raw pointer in a free_pointer.
 template<typename T>
 free_pointer<T> wrap_freeing(T * pointer) {
 	return free_pointer<T>{pointer};
